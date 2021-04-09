5f76700a27f1b4fb40c01c9ad862551b37ecaff8
pkienscherf <philipp.kienscherf@gmail.com>
2021-04-09 15:19:35 +0200

    Fix typo

--- 09_Python_Software_Projects/prodas/setup.py
+++ 09_Python_Software_Projects/prodas/setup.py
@@ -6,7 +6,9 @@ setup(
     description="Programming Data Science",
     author="Philipp",
     packages=["prodas"],
-    entry_point={
-        'console_scripts': ['prodas_cli=prodas.cli:main']
+    entry_points={
+        'console_scripts': [
+            'prodas = prodas.cli:main'
+        ]
     }
 )
